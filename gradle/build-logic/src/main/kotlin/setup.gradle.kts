033320fbccfe0be4ab30aa039c40069c88bdab26
Philip Wedemann <22521688+hfhbd@users.noreply.github.com>
2026-01-30 14:32:12 +0100

    Fix repo url

--- gradle/build-logic/src/main/kotlin/setup.gradle.kts
+++ gradle/build-logic/src/main/kotlin/setup.gradle.kts
@@ -33,16 +33,16 @@ configurations.archives {
 
 publishing {
     repositories {
-        maven(url = "https://maven.pkg.github.com/hfhbd/githubreleases") {
+        maven(url = "https://maven.pkg.github.com/hfhbd/github-releases") {
             name = "GitHubPackages"
             credentials(PasswordCredentials::class)
         }
     }
     publications.withType<MavenPublication>().configureEach {
         pom {
-            name.set("hfhbd githubreleases")
-            description.set("hfhbd githubreleases")
-            url.set("https://github.com/hfhbd/githubreleases")
+            name.set("hfhbd github-releases")
+            description.set("hfhbd github-releases")
+            url.set("https://github.com/hfhbd/github-releases")
             licenses {
                 license {
                     name.set("Apache-2.0")
@@ -57,15 +57,15 @@ publishing {
                 }
             }
             scm {
-                connection.set("scm:git://github.com/hfhbd/githubreleases.git")
-                developerConnection.set("scm:git://github.com/hfhbd/githubreleases.git")
+                connection.set("scm:git://github.com/hfhbd/github-releases.git")
+                developerConnection.set("scm:git://github.com/hfhbd/github-releases.git")
                 url.set("https://github.com/hfhbd/githubreleases")
             }
             distributionManagement {
                 repository {
                     id = "github"
                     name = "GitHub hfhbd Apache Maven Packages"
-                    url = "https://maven.pkg.github.com/hfhbd/githubreleases"
+                    url = "https://maven.pkg.github.com/hfhbd/github-releases"
                 }
             }
         }
